4d99fc9b0ef8da52d5ac08117a7e02ec1933e014
Dean Johnson <deanjohnson222@gmail.com>
2014-02-20 19:18:18 -0800

    Fix bugs.

--- usercreate.sql
+++ usercreate.sql
@@ -1,5 +1,5 @@
 CREATE USER 'vagrant'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON 'nobel.*' TO 'vagrant'@'localhost' with GRANT OPTION;
+GRANT ALL PRIVILEGES ON nobel.* TO 'vagrant'@'localhost' with GRANT OPTION;
 CREATE database systemview;
-GRANT ALL PRIVILEGES ON 'systemview.*' TO 'vagrant'@'localhost' with GRANT OPTION;
+GRANT ALL PRIVILEGES ON systemview.* TO 'vagrant'@'localhost' with GRANT OPTION;
 
