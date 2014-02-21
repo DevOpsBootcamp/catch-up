f4804c5624d61179fa888df15adc80bb9cce4c5f
Dean Johnson <deanjohnson222@gmail.com>
2014-02-20 19:49:39 -0800

    Fixed user permissions in the sql script.

--- usercreate.sql
+++ usercreate.sql
@@ -1,7 +1,7 @@
-drop user vagrant@localhost;
-flush privileges;
+DROP USER vagrant@localhost;
+FLUSH PRIVILEGES;
 CREATE USER 'vagrant'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON nobel.* TO 'vagrant'@'localhost' with GRANT OPTION;
+GRANT ALL PRIVILEGES ON nobel.* TO 'vagrant'@'localhost' WITH GRANT OPTION;
+DROP database systemview;
 CREATE database systemview;
-GRANT ALL PRIVILEGES ON systemview.* TO 'vagrant'@'localhost' with GRANT OPTION;
-
+GRANT ALL PRIVILEGES ON systemview.* TO 'vagrant'@'localhost' WITH GRANT OPTION;
