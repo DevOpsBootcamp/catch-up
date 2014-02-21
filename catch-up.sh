4f72fa1f94f838832cdbc5acce0cb5d1cf8a69fc
Dean Johnson <deanjohnson222@gmail.com>
2014-02-20 18:17:27 -0800

    Updated script for Lesson 08.
    I also added in a few fixes for Lesson 05.

--- catch-up.sh
+++ catch-up.sh
@@ -25,10 +25,12 @@ fi
 
 if [ $1 -gt 4 ]; then
     echo "Updating for Lesson 05."
+    sudo useradd vagrant
     sudo yum install python-virtualenv*
     virtualenv ~/systemview_venv
     source ~/systemview_venv/bin/activate
-    git clone git@github.com:DevOpsBootcamp/systemview.git ~
+    cd ~
+    git clone https://github.com/DevOpsBootcamp/systemview.git
     pip install flask
     pip install argparse
 fi
@@ -36,3 +38,22 @@ fi
 if [ $1 -gt 5 ]; then
     echo "Nothing to do for Lesson 06."
 fi
+
+if [ $1 -gt 6 ]; then
+    echo "Updating for Lesson 07."
+    sudo yum install mysql-server
+    sudo /sbin/service mysqld start
+    sudo /usr/bin/mysql_secure_installation
+    sudo mysqladmin -p create nobel
+    sudo mysql < ~/catch-up/usercreate.sql
+    wget http://osl.io/nobel -O nobel.sql
+    mysql -p nobel < nobel.sql
+    sudo yum install python-devel
+    sudo yum install mysql-devel
+    cd ~
+    source ~/systemview_venv/bin/activate
+    cd ~/systemview
+    git checkout -tb save-search origin/save-search
+    pip install -r requirements.txt
+fi
+
---
+++ usercreate.sql
@@ -0,0 +1,5 @@
+CREATE USER 'vagrant'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON 'nobel.*' TO 'vagrant'@'localhost' with GRANT OPTION;
+CREATE database systemview;
+GRANT ALL PRIVILEGES ON 'systemview.*' TO 'vagrant'@'localhost' with GRANT OPTION;
+
